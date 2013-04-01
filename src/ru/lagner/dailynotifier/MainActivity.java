41f71d511f82984760a20b7cbf8547714bfa26ba
bumbaram <spam@lagner.ru>
2013-04-02 01:28:09 +0700

    TimeReceiver works. Last activity is updated

--- src/ru/lagner/dailynotifier/MainActivity.java
+++ src/ru/lagner/dailynotifier/MainActivity.java
@@ -1,15 +1,29 @@
 package ru.lagner.dailynotifier;
 
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	public static final String SETTINGS = "appSettings";
+	public static final String LAST_ACTIVITY = "lastactivity";
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+				
+		context = getApplicationContext();
+		// put now to the last activity time
+		updateLastActivityTime(context);
 	}
 
 	@Override
@@ -18,5 +32,24 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	
+	public static void updateLastActivityTime(Context context)
+	{
+		SharedPreferences pref = context.getSharedPreferences(SETTINGS, MODE_PRIVATE);
+		Date now = new Date();
+		
+		Editor editor = pref.edit();
+		editor.putLong(LAST_ACTIVITY, now.getTime());
+		editor.commit();
+		
+		Log.i(logTag, "Last activity time was updated to " + now.toString());
+	}
+	
+	
+	// Private
+	
+	private Context context;
+	
+	private static final String logTag = "MainActivity";
 }
--- src/ru/lagner/dailynotifier/TimeReceiver.java
+++ src/ru/lagner/dailynotifier/TimeReceiver.java
@@ -1,18 +1,22 @@
 package ru.lagner.dailynotifier;
 
+import java.util.Date;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.util.Log;
 
 public class TimeReceiver extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(Context arg0, Intent arg1) {
+	public void onReceive(Context context, Intent intent) {
 		// TODO Auto-generated method stub
 		
-		Log.i(logTag, "time was changed -> reset last activity time");
-			
+		Log.i(logTag, "global time was changed -> reset last activity time");		
+		MainActivity.updateLastActivityTime(context);		
 	}
 	
 	final private String logTag = "TimeReceiver";
